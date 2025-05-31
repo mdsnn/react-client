d5f53646def03114cf92eb489080cb32998bbca3
mwiko <mwikosinyangwe@gmail.com>
2025-05-31 17:11:07 +0200

    questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Quiz from "./components/Quiz";
 
 function App() {
   return (
     <div className="app-container">
       <h1>Quiz App</h1>
+      <Quiz />
     </div>
   );
 }
---
+++ src/components/Quiz.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Quiz: React.FC = () => {
+  const questioBank = [
+    {
+      question: "What is the capital of France?",
+      options: ["Berlin", "London", "Paris", "Rome"],
+      answer: "Paris",
+    },
+    {
+      question: "Which language is used for web apps?",
+      options: ["PHP", "Python", "Javascript", "All"],
+      answer: "All",
+    },
+    {
+      question: "What does JSX stand for?",
+      options: [
+        "JavaScript XML",
+        "Java Syntax eXtension",
+        "Just a Simple eXample",
+        "None of the above",
+      ],
+      answer: "JavaScript XML",
+    },
+  ];
+  return (
+    <div>
+      <h2>Question 1</h2>
+      <p className="question">{questioBank[0].question}</p>
+      {questioBank[0].options.map((option) => (
+        <button className="option">{option}</button>
+      ))}
+      <div className="nav-buttons">
+        <button> Previous </button>
+        <button> Next </button>
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;
